ee3edc92076cb126766960a4c027467cd44a32b5
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-11-10 16:35:39 +0000

    adds actnorm but without minibatch init yet

--- glow/actnorm.py
+++ glow/actnorm.py
@@ -5,11 +5,18 @@ import torch.nn.functional as F
 from .utils import HelperModule
 
 class ActNorm(HelperModule):
-    def build(self):
-        pass
+    def build(self, 
+            nb_channels: int, 
+        ):
+        self.loc = nn.Parameter(torch.zeros(1, nb_channels, 1, 1))
+        self.scale = nn.Parameter(torch.ones(1, nb_channels, 1, 1))
 
-    def forward(self):
-        pass
+        # TODO: do init function
+        
+    # TODO: do I need to return logdet?
+    def forward(self, x):
+        _, _, hx, wx = x.shape
+        return self.scale * (x + self.loc), hw * wx * self.scale.abs().log().sum()
 
-    def reverse(self):
-        pass
+    def reverse(self, x):
+        return x / self.scale - self.loc
