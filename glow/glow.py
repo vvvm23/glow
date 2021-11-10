b11d46eecff193965bbface77d25cfdc6201cd4a
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-11-10 21:33:25 +0000

    adds `Flow` module

--- glow/glow.py
+++ glow/glow.py
@@ -4,18 +4,30 @@ import torch.nn.functional as F
 
 from .utils import HelperModule
 from .actnorm import ActNorm
-from .coupling import AdditiveCoupling, AffineCoupling
+from .coupling import AffineCoupling
 from .conv import InvConv
 
 class Flow(HelperModule):
-    def build(self):
-        pass
+    def build(self, 
+            nb_channels: int,
+            lu: bool = True,
+        ):
+        self.norm = ActNorm(nb_channels)
+        self.conv = InvConv(nb_channels, lu)
+        self.coupling = AffineCoupling(nb_channels)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x, d1 = self.norm(x)
+        x, d2 = self.conv(x)
+        x, d3 = self.coupling(x)
 
-    def reverse(self):
-        pass
+        return x, d1 + d2 + d3
+
+    def reverse(self, x):
+        x = self.coupling.reverse(x)
+        x = self.conv.reverse(x)
+        x = self.norm.reverse(x)
+        return x
 
 class Glow(HelperModule):
     def build(self):
@@ -26,3 +38,11 @@ class Glow(HelperModule):
 
     def reverse(self):
         pass
+
+if __name__ == '__main__':
+    flow = Flow(8, lu=False)
+    x = torch.randn(4, 8, 16, 16)
+    y, logdet = flow(x)
+    xr = flow.reverse(y)
+    print(x[0,0])
+    print(xr[0,0])
