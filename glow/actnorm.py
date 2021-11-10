424dd40aec3f865b8929919062fff7f9d7a8e391
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-11-10 20:26:49 +0000

    adds init to `ActNorm`

--- glow/actnorm.py
+++ glow/actnorm.py
@@ -11,10 +11,26 @@ class ActNorm(HelperModule):
         self.loc = nn.Parameter(torch.zeros(1, nb_channels, 1, 1))
         self.scale = nn.Parameter(torch.ones(1, nb_channels, 1, 1))
 
-        # TODO: do init function
+        self.register_buffer('initialized', torch.tensor(0, dtype=torch.uint8))
+
+    @torch.no_grad()
+    def intialize(self, x):
+        _, c, _ _ = x.shape
+        flatten = x.permute(1, 0, 2, 3).view(c, -1)
+
+        mean = flatten.mean(-1)[..., None, None, None].permute(1, 0, 2, 3)
+        std = flatten.std(-1)[..., None, None, None].permute(1, 0, 2, 3)
+
+        self.loc.data.copy_(-mean)
+        self.scale.data.copy_(1 / (std + 1e-6))
+
+        self.initialized.fill_(1)
         
     # TODO: do I need to return logdet?
     def forward(self, x):
+        if not self.initialized.item():
+            self.intialize(x)
+
         _, _, hx, wx = x.shape
         return self.scale * (x + self.loc), hw * wx * self.scale.abs().log().sum()
 
